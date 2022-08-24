a2b590d4583fcb3d337a8ffc324c4b5af0a16257
Christof Marti <chrmarti@microsoft.com>
2022-08-24 07:46:41 +0000

    ...

--- src/color/install.sh
+++ src/color/install.sh
@@ -8,7 +8,7 @@ echo "The provided favorite color is: ${FAVORITE}"
 tee /usr/local/bin/color > /dev/null \
 << EOF
 #!/bin/sh
-echo "my favorite color is ${FAVORITE} (hello 12.34.56)"
+echo "my favorite color is ${FAVORITE} (color 12.34.56)"
 EOF
 
 chmod +x /usr/local/bin/color
