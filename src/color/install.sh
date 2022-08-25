200b405e82723b2a0954eaa56a31a8ae56a590e6
Christof Marti <chrmarti@microsoft.com>
2022-08-25 09:14:50 +0000

    update

--- src/color/install.sh
+++ src/color/install.sh
@@ -3,12 +3,13 @@
 set -e
 
 echo "Activating feature 'color'"
+
 echo "The provided favorite color is: ${FAVORITE}"
 
-tee /usr/local/bin/color > /dev/null \
+cat > /usr/local/bin/color \
 << EOF
 #!/bin/sh
-echo "my favorite color is ${FAVORITE} (color 12.34.56)"
+echo "my favorite color is ${FAVORITE} (color 12.34.567)"
 EOF
 
 chmod +x /usr/local/bin/color
--- src/hello/install.sh
+++ src/hello/install.sh
@@ -7,12 +7,12 @@ echo "Activating feature 'hello'"
 GREETING=${GREETING:-undefined}
 echo "The provided greeting is: $GREETING"
 
-tee /usr/local/bin/hello > /dev/null \
+cat > /usr/local/bin/hello \
 << EOF
 #!/bin/sh
 RED='\033[0;91m'
 NC='\033[0m' # No Color
-echo "\${RED}${GREETING}, \$(whoami)!\${NC} (hello 2.5.6)"
+echo "\${RED}${GREETING}, \$(whoami)!\${NC} (hello 2.5.67)"
 EOF
 
 chmod +x /usr/local/bin/hello
